0474f270abfbdd340dd4807d12ba1ef72c06ea30
Ian Earley <iearley88@gmail.com>
2018-05-29 15:16:28 -0600

    fleshing out form

--- src/components/madlib_form.js
+++ src/components/madlib_form.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
+import {
+  Col,
+  Row
+} from 'reactStrap';
 
 class MadLibForm extends Component {
+  handleChange = function() {
+    console.log('trying to handle change')
+  }
   render() {
-    return <h1> Bottega MadLib Form Component </h1>;
+    return(
+      <Row style={{textAlign: 'center', color: 'white'}}>
+        <Col md="3">Column
+          <Row>
+            <Col md="2">
+              <label classname="greenLabel">1</label>
+            </Col>
+            <Col md="10">
+              <input placeholder="Color" type="text" onChange={this.handleChange}/>
+            </Col>
+          </Row>
+        </Col>
+        <Col md="3">Column
+          <Row>
+            <Col md="2">
+              <label classname="greenLabel">2</label>
+            </Col>
+            <Col md="10">
+              <input placeholder="Color" type="text" onChange={this.handleChange}/>
+            </Col>
+          </Row>
+        </Col>
+        <Col md="3">Column</Col>
+        <Col md="3">Column</Col>
+      </Row>
+    );
   }
 }
 
